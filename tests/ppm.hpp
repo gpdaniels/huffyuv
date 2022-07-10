d2914433686ed5f0c8828110e771d77ffdbbc96d
Geoffrey Daniels <geoff@gpdaniels.com>
2022-07-10 17:04:04 +0100

    Adding missing std:: within load_ppm function.

--- tests/ppm.hpp
+++ tests/ppm.hpp
@@ -5,19 +5,19 @@
 #include <string>
 
 std::unique_ptr<unsigned char[]> load_ppm(const std::string& filepath, int& width, int& height) {
-    FILE* handle = fopen(filepath.c_str(), "rb");
+    std::FILE* handle = std::fopen(filepath.c_str(), "rb");
     if (!handle) {
         return nullptr;
     }
     char P6[2]{};
-    fscanf(handle, "%c%c", &P6[0], &P6[1]);
+    std::fscanf(handle, "%c%c", &P6[0], &P6[1]);
     if ((P6[0] != 'P') || (P6[1] != '6')) {
         fclose(handle);
         return nullptr;
     }
-    fscanf(handle, "%d\n %d\n", &width, &height);
+    std::fscanf(handle, "%d\n %d\n", &width, &height);
     int maximum;
-    fscanf(handle, "%d\n", &maximum);
+    std::fscanf(handle, "%d\n", &maximum);
     const size_t size = width * height;
     std::unique_ptr<unsigned char[]> pixels = std::unique_ptr<unsigned char[]>(new unsigned char[size]);
     size_t index = 0;
@@ -31,6 +31,6 @@ std::unique_ptr<unsigned char[]> load_ppm(const std::string& filepath, int& widt
     if (index != size) {
         return nullptr;
     }
-    fclose(handle);
+    std::fclose(handle);
 	return pixels;
 }
