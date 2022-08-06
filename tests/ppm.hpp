8693fe1dbfde6d968102b50644a278a29126600b
Geoffrey Daniels <geoff@gpdaniels.com>
2022-08-06 23:44:52 +0100

    Fixing missing channels multiplier for ppm reader and writer.

--- tests/ppm.hpp
+++ tests/ppm.hpp
@@ -21,7 +21,7 @@ bool ppm_save(const std::string& filepath, int width, int height, const unsigned
         fclose(handle);
         return false;
     }
-    const size_t size = width * height;
+    const size_t size = width * height * 3;
     size_t index = 0;
     while (index < size) {
         const size_t delta = std::fwrite(&pixels[index], sizeof(char), size - index, handle);
@@ -51,7 +51,7 @@ std::unique_ptr<unsigned char[]> ppm_load(const std::string& filepath, int& widt
     std::fscanf(handle, "%d %d\n", &width, &height);
     int maximum;
     std::fscanf(handle, "%d\n", &maximum);
-    const size_t size = width * height;
+    const size_t size = width * height * 3;
     std::unique_ptr<unsigned char[]> pixels = std::unique_ptr<unsigned char[]>(new unsigned char[size]);
     size_t index = 0;
     while (index < size) {
